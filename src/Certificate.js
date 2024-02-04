2ec42c6db692ab8f1ddd4fbc566caffe832f0b0d
Gopikrishna S P <krish.official365@gmail.com>
2024-02-04 15:12:38 +0530

    Initial commit

---
+++ src/Certificate.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Certificate() {
+    return (
+        <>
+        <div className='certificate'>
+            <div className='certificate-content'>
+            <h3>Certificates</h3>
+                <p className='certificate-name'>Java Full-Stack Development</p>
+                <p className='certificate-detail'>Acquired in-depth knowledge of Java. Developed proficiency in using React.js and Spring Boot framework. Hands-on experience in building full stack applications. Engaged in collaborative coding practices, version control using Git.</p>
+                <p className='certificate-name'>Programming In Java - NPTEL(National Programme on Technology Enhanced Learning) 2022</p>
+                <p className='certificate-detail'>Completing the ‘Programming in Java’ course provided me with comprehensive knowledge in object-oriented programming, allowing me to develop applications, manage data structure and implement algorithms using java.</p>
+            </div>
+        </div>    
+        </>
+    );
+}
+
+export default Certificate;
